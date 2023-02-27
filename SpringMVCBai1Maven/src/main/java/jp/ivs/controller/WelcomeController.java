bac50c8767c8007e5fa26f8d114d0d9d0e875eaa
DESKTOP-6KHCC5V\Ron <truong.pn.62cntt@ntu.edu.vn>
2023-02-27 10:32:26 +0700

    27/2/2023

---
+++ SpringMVCBai1Maven/src/main/java/jp/ivs/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package jp.ivs.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+
+public class WelcomeController{
+	@RequestMapping("/Welcome")
+	public String Welcome() {
+		return "viewWelcome";
+	}
+	
+}
---
+++ SpringMVCBai1Maven/src/main/webapp/WEB-INF/views/viewWelcome.jsp
@@ -0,0 +1,11 @@
+<%@ page pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	Chào Mừng MVC
+</body>
+</html>
